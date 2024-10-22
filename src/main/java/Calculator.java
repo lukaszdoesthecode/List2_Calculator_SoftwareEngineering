f850e49554f67a7dbef24a4680e566bc24047843
Lukasz Stecyk <stecyk.lukasz@o2.pl>
2024-10-22 14:55:43 +0200

    Created calculator class; Addition method added

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    /** Function adding two floating-point numbers
+     *
+     * @param a - fist parameter (float)
+     * @param b - second parameter (float)
+     * @return - sum of both numbers
+     */
+    public float add(float a, float b){
+        return a+b;
+    }
+}
